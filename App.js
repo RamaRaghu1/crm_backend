13e7c07b4184c2634b826c45e2ca51f31526232f
Rama Raghu <ramanadar003@gmail.com>
2025-01-02 17:36:36 +0530

    commit changes

--- App.js
+++ App.js
@@ -21,7 +21,7 @@ const corsOptions = {
   optionsSuccessStatus: 204
 };
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "https://hrm.kairaatechserve.com/,http://localhost:5173/");
+  res.header("Access-Control-Allow-Origin", "https://hrm.kairaatechserve.com/");
   res.header("Access-Control-Allow-Credentials", "true");
   res.header("Access-Control-Allow-Methods", "PUT,POST,PATCH,DELETE,GET");
   res.header("Access-Control-Allow-Headers", "Origin,X-Requested-With,Content-Type,Accept,Authorization");
