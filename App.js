4618ede828d8eabcbe3dd717c0712ee5e22c4c36
Rama Raghu <ramanadar003@gmail.com>
2025-01-02 17:22:06 +0530

    commit changes

--- App.js
+++ App.js
@@ -27,7 +27,7 @@ app.use((req, res, next) => {
   return next()
 })
 
-app.use(cors());
+// app.use(cors());
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(cookieParser());
